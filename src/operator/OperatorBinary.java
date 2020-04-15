a973ff162e41762354d8828b92dad314a12c5032
raflifirmansyah <rafifirman198@gmail.com>
2020-04-15 20:04:18 +0800

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijadikan oleh program
+
+    String nama = "Muhammad Rafli Firmansyah";
+    int jumlahsks = 24;
+    double IndeksPresentasiKumulatif = 3.55;
+    
+}
