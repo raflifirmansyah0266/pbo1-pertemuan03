4dfa462e77ef43ab5e0566b105b4d71948a12c8a
raflifirmansyah <rafifirman198@gmail.com>
2020-04-15 21:20:08 +0800

    percabangan switch

---
+++ src/operator/Switch.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class Switch {
+    public static void main(String[] args){
+        int jalanjalan=1;
+        switch (jalanjalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+            default:
+                System.out.println("mahkluk halus");
+                break;
+        }
+
+    }
+}
