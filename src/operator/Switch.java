278723fcfb4d33555fb31e40cc45ea5425328245
raflifirmansyah <rafifirman198@gmail.com>
2020-04-15 21:34:46 +0800

    percabangan switch dengan char

--- src/operator/Switch.java
+++ src/operator/Switch.java
@@ -2,21 +2,27 @@ package operator;
 
 public class Switch {
     public static void main(String[] args){
-        int jalanjalan=1;
-        switch (jalanjalan){
-            case 1:
-                System.out.println("urang lapas");
+        char nilai='A';
+        switch (nilai){
+            case 'A':
+                System.out.println("Mhs   :\"Terimakasih pak\"");
+                System.out.println("Dosen :\"Selamat ya!!\"");
                 break;
-            case 2:
-                System.out.println("urang acayan");
+            case 'B':
+                System.out.println("Mhs   :\"kenapa ulun kada dapat A, pak?\"");
+                System.out.println("dosen :\"#$%@\"");
                 break;
-            case 3:
-                System.out.println("ubat nyamuk");
+            case 'C':
+                System.out.println("Mhs   :\"Ulun turun pul pak ai, tugas ngumpul jua\"");
+                System.out.println("dosen :\"Tapi bisalah menjawab ujian\"");
+                System.out.println("Mhs   :\"wkwkwkw... kada pa ai\"");
                 break;
-            case 4:
-                System.out.println("dubil dit");
             default:
-                System.out.println("mahkluk halus");
+                System.out.println("Mhs   :\"Ulun turun pul pak ai, tugas ngumpul jua\"");
+                System.out.println("Dosem :\"Bujur juakah\"");
+                System.out.println("Dosen :\"memeriksa berkasas\"");
+                System.out.println("Dosen :\"menceleng\"");
+                System.out.println("Mhs   :\"Kabur..\"");
                 break;
         }
 
